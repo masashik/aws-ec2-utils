f757f8faceee89000f0b5c64d1de3e680bac005e
Masashi Kobayashi <masashi@masashi.ca>
2025-07-04 21:27:47 -0400

    2 scripts added to start and stop all regional group of EC2 instances.

---
+++ ec2_start.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import boto3
+from botocore.exceptions import ClientError
+import argparse
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+# Shutdown all EC2 instances in a specified region.
+def start_ec2_instances(region):
+    try:
+        ec2 = boto3.client("ec2", region_name=region)
+        response = ec2.describe_instances()
+    except ClientError as e:
+        logging.error(f"Failed to connect to EC2: {e}")
+        return
+
+    instance_IDs = []
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            instance_IDs.append(instance_id)
+
+    try:
+        ec2 = boto3.client("ec2", region_name=region)
+        response = ec2.start_instances(InstanceIds=instance_IDs)
+    except ClientError as e:
+        logging.error(f"Failed to connect to EC2: {e}")
+        return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="List EC2 instances and their metadata.")
+    parser.add_argument("--region", required=True, help="AWS region, e.g. us-west-2")
+    args = parser.parse_args()
+
+    start_ec2_instances(args.region)
