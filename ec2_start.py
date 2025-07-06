e73b3024d3c5a4877726ba355cad6175c5fc0f21
Masashi Kobayashi <masashi@masashi.ca>
2025-07-05 21:32:05 -0400

    Refactored ec2_start.py to utilize ec2_ops.py which has general operations of EC2 instances.

--- ec2_start.py
+++ ec2_start.py
@@ -4,35 +4,28 @@ import boto3
 from botocore.exceptions import ClientError
 import argparse
 import logging
+from ec2_utils.ec2_ops import get_ec2_instances, filter_instances, start_instances
 
 # Setup logging
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 
-# Shutdown all EC2 instances in a specified region.
+# Start all EC2 instances in a specified region.
 def start_ec2_instances(region):
-    try:
-        ec2 = boto3.client("ec2", region_name=region)
-        response = ec2.describe_instances()
-    except ClientError as e:
-        logging.error(f"Failed to connect to EC2: {e}")
-        return
-
-    instance_IDs = []
-    for reservation in response["Reservations"]:
-        for instance in reservation["Instances"]:
-            instance_id = instance["InstanceId"]
-            instance_IDs.append(instance_id)
-
-    try:
-        ec2 = boto3.client("ec2", region_name=region)
-        response = ec2.start_instances(InstanceIds=instance_IDs)
-    except ClientError as e:
-        logging.error(f"Failed to connect to EC2: {e}")
-        return
-
-if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="List EC2 instances and their metadata.")
     parser.add_argument("--region", required=True, help="AWS region, e.g. us-west-2")
+    parser.add_argument("--tag-key", help="Tag key to filter")
+    parser.add_argument("--tag-value", help="Tag value to filter")
+    parser.add_argument("--dry-run", action="store_true", help="Dry run only")
     args = parser.parse_args()
 
-    start_ec2_instances(args.region)
+    instances = get_ec2_instances(args.region)
+    filtered = filter_instances(instances,args.tag_key,args.tag_value)
+
+    instance_ids = []
+    for i in filtered:
+        instance_ids.append(i["InstanceId"])
+
+    start_instances(args.region, instance_ids, dry_run=args.dry_run)
+
+if __name__ == "__main__":
+    start_ec2_instances()
