8d2e8061ce83a2304489dbd510cd95b1d3072929
Masashi Kobayashi <masashi@masashi.ca>
2025-07-20 09:19:54 -0400

    Adding CloudWatch enabling packages and open port 80 to make HTTP available.

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -5,6 +5,17 @@ resource "aws_instance" "dev_ec2" {
   subnet_id              = var.subnet_id  # <-- Replace with your subnet ID
   key_name               = var.key_name    # <-- Replace with your SSH key name
   vpc_security_group_ids = var.vpc_security_group_ids
+  iam_instance_profile   = "CloudWatchEC2"
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    sudo yum install -y perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https perl-Digest-SHA.x86_64
+    cd /home/ec2-user/
+    curl https://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.2.zip -O
+    unzip CloudWatchMonitoringScripts-1.2.2.zip
+    rm -rf CloudWatchMonitoringScripts-1.2.2.zip
+  EOF
 
   tags = {
     Name = "dev-server"
