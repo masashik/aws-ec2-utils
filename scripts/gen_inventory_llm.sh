51d8044bd0825bc5327ae95d44efbff2863cace1
Masashi Kobayashi <masashi@masashi.ca>
2025-08-21 23:04:03 -0400

    Correct hosts from dev to llm_ec2.

---
+++ scripts/gen_inventory_llm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ../tofu/ec2
+
+IPS=$(tofu output -json instance_ips | jq -r '.[]')
+
+cd ../..
+
+cat <<EOF > ansible/inventory.ini
+[llm_ec2]
+EOF
+
+for ip in $IPS; do
+  echo "$ip ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/$AWS_EC2_SSH_KEY_NAME" >> ansible/inventory.ini
+done
+
+echo "[OK] Inventory generated at ansible/inventory.ini"
