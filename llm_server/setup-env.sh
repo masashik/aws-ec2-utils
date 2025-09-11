36369ba5b60c4ab7c12d05c916ca8db01a517344
Masashi Kobayashi <masashi@masashi.ca>
2025-09-11 19:40:54 -0400

    (completed) Migrate LLM API server from AWS EC2 to EKS.
        Minor correction regarding Ollama LLM model, AWS ECR image URI,
        and updated a way to export environment values.

--- llm_server/setup-env.sh
+++ llm_server/setup-env.sh
@@ -1,6 +1,7 @@
-AWS_REGION=ca-central-1
-ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-APP_NAME=aws-ec2-utils/llm-api-server
-ECR_REPO=${APP_NAME}
-CLUSTER_NAME=llm-eks
-K8S_NAMESPACE=llm
+export AWS_REGION=ca-central-1
+export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+export APP_NAME=llm-api-server
+export ECR_REPO=${APP_NAME}
+export CLUSTER_NAME=llm-eks
+export K8S_NAMESPACE=llm
+export POLICY_URL="https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
